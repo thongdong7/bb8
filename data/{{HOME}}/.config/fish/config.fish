22f324dc0aa852a72e561ab5a66f6e8f5de60438
Thong Dong <thongdong7@gmail.com>
2016-06-09 13:49:11 +0700

    add decor template

--- data/{{HOME}}/.config/fish/config.fish
+++ data/{{HOME}}/.config/fish/config.fish
@@ -1,3 +1,4 @@
 set -gx PATH ~/dev/bin ~/dev3/bin $PATH
+set -gx PATH /data/software/node-v4.1.0-linux-x64/bin $PATH
 set -gx PATH ~/Android/Sdk/platform-tools $PATH
 set -gx ANDROID_HOME ~/Android/Sdk
